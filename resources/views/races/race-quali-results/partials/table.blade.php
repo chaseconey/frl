858187cae818367af67eb9121fed963795b08c7e
Chase Coney <chasiepoo@gmail.com>
2020-12-22 16:33:38 -0600

    fix(quali-results): remove unused edit columns

--- resources/views/races/race-quali-results/partials/table.blade.php
+++ resources/views/races/race-quali-results/partials/table.blade.php
@@ -24,9 +24,6 @@
                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                             Best Sector Times
                         </th>
-                        <th scope="col" class="relative px-6 py-3">
-                            <span class="sr-only">Edit</span>
-                        </th>
                     </tr>
                     </thead>
                     <tbody class="bg-white divide-y divide-gray-200">
