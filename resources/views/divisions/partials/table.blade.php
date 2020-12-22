da58a4c48cc318a54ff282ee2dad7dd88480d857
Chase Coney <chasiepoo@gmail.com>
2020-12-22 17:38:35 -0600

    feat(roster): add driver management link for admins

--- resources/views/divisions/partials/table.blade.php
+++ resources/views/divisions/partials/table.blade.php
@@ -97,6 +97,13 @@
                                             <a href="#"
                                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                                                role="menuitem">Driver Profile</a>
+
+                                            @role('admin')
+                                                <a href="/nova/resources/drivers/{{ $driver->id }}"
+                                                   target="_blank"
+                                                   class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                                                   role="menuitem">Manage Driver</a>
+                                            @endrole
                                         </div>
                                     </div>
                                 </div>
