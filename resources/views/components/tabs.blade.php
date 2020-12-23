9afbaa1d23da89f5ee5f573f4a48a7d84103bebd
Chase Coney <chasiepoo@gmail.com>
2020-12-22 20:12:00 -0600

    feat(tabs): mobile select links
    closes #5

--- resources/views/components/tabs.blade.php
+++ resources/views/components/tabs.blade.php
@@ -1,9 +1,14 @@
 <div>
     <div class="sm:hidden">
         <label for="tabs" class="sr-only">Select a tab</label>
-        <select id="tabs" name="tabs" class="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+        <select
+            id="tabs"
+            name="tabs"
+            class="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            onchange="location = this.value;"
+        >
             @foreach ($links as $link)
-            <option>{{ $link['title'] }}</option>
+            <option value="{{ $link['link'] }}">{{ $link['title'] }}</option>
             @endforeach
         </select>
     </div>
