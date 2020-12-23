479f76e86075ceed85af0c8501ff6c82fdeab26e
Chase Coney <chasiepoo@gmail.com>
2020-12-22 19:42:33 -0600

    feat(signup): don't allow multiple signups per div

--- resources/views/signup/create.blade.php
+++ resources/views/signup/create.blade.php
@@ -10,7 +10,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    @include('signup.partials.form')
+
+                    @php
+                        $divDriver = auth()->user()->drivers->where('division_id', $division->id)->first()
+                    @endphp
+
+                    @if(!$divDriver)
+                        @include('signup.partials.form')
+                    @else
+                        <x-alert-warning>
+                            <span>You already are enrolled in {{ $division->name }} as {{ $divDriver->name }}</span>
+                            <p class="text-sm font-medium underline text-yellow-700 hover:text-yellow-600"><a class="underline" href="{{ route('signup.index') }}">Try Another Division</a></p>
+                        </x-alert-warning>
+                    @endif
                 </div>
             </div>
         </div>
