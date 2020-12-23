78e1083048a5a983c52fcd9696256d3cd1cbe766
Chase Coney <chasiepoo@gmail.com>
2020-12-23 12:20:47 -0600

    feat(matrix): optimize f1Number query

--- app/Http/Controllers/Division/StandingController.php
+++ app/Http/Controllers/Division/StandingController.php
@@ -53,7 +53,7 @@ class StandingController extends Controller
     {
         // TODO: Divisions need date constraints (season? end time?)
         $standings = Driver::where('division_id', $division->id)
-            ->with('user', 'f1Team', 'raceResults')
+            ->with('user', 'f1Team', 'f1Number', 'raceResults')
             ->has('raceResults')
             ->withSum('raceResults', 'points')
             ->orderByDesc('race_results_sum_points')
