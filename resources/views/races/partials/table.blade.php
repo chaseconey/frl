7af5ee47877bfbf7a7eb44562598b06e3454a620
Chase Coney <chasiepoo@gmail.com>
2020-12-23 15:19:31 -0600

    style(races): add broadcast symbol

--- resources/views/races/partials/table.blade.php
+++ resources/views/races/partials/table.blade.php
@@ -19,7 +19,7 @@
                             Status
                         </th>
                         <th scope="col" class="relative px-6 py-3">
-                            <span class="sr-only">Edit</span>
+                            <span class="sr-only">Actions</span>
                         </th>
                     </tr>
                     </thead>
@@ -33,7 +33,9 @@
                                 </div>
                                 <div class="ml-4">
                                     <div class="text-sm font-medium text-gray-900">
-                                        {{ $race->track->name }}
+                                        <a href="{{ route('race.results.index', $race->id) }}" class="text-indigo-600 hover:text-indigo-900">
+                                            {{ $race->track->name }}
+                                        </a>
                                     </div>
                                     <div class="text-sm text-gray-500">
                                         {{ country_code_to_name($race->track->country) }}
@@ -54,8 +56,12 @@
                             </span>
                         </td>
                         <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                            @if($race->race_time->isPast())
-                            <a href="{{ route('race.results.index', $race->id) }}" class="text-indigo-600 hover:text-indigo-900">Results</a>
+                            @if($race->broadcast_id)
+                            <a href="{{ route('races.broadcast', $race->id) }}" class="text-indigo-600 hover:text-indigo-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
+                                </svg>
+                            </a>
                             @endif
                         </td>
                     </tr>
