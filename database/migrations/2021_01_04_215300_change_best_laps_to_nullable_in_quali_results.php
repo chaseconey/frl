be6ba4fbc59897a8be88c1bb660ccd856ab3a221
Chase Coney <chasiepoo@gmail.com>
2021-01-04 16:24:46 -0600

    feat(quali-results): make most data nullable as laps are not required in quali

---
+++ database/migrations/2021_01_04_215300_change_best_laps_to_nullable_in_quali_results.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeBestLapsToNullableInQualiResults extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('race_quali_results', function (Blueprint $table) {
+            $table->float('best_s1_time', 8, 3)->nullable()->change();
+            $table->float('best_s2_time', 8, 3)->nullable()->change();
+            $table->float('best_s3_time', 8, 3)->nullable()->change();
+            $table->float('lap_delta', 8, 3)->nullable()->change();
+            $table->float('best_s1_delta', 8, 3)->nullable()->change();
+            $table->float('best_s2_delta', 8, 3)->nullable()->change();
+            $table->float('best_s3_delta', 8, 3)->nullable()->change();
+            $table->float('speedtrap_speed', 8, 3)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('race_quali_results', function (Blueprint $table) {
+            $table->float('best_s1_time', 8, 3)->nullable(false)->change();
+            $table->float('best_s2_time', 8, 3)->nullable(false)->change();
+            $table->float('best_s3_time', 8, 3)->nullable(false)->change();
+            $table->float('lap_delta', 8, 3)->nullable(false)->change();
+            $table->float('best_s1_delta', 8, 3)->nullable(false)->change();
+            $table->float('best_s2_delta', 8, 3)->nullable(false)->change();
+            $table->float('best_s3_delta', 8, 3)->nullable(false)->change();
+            $table->float('speedtrap_speed', 8, 3)->nullable(false)->change();
+        });
+    }
+}
