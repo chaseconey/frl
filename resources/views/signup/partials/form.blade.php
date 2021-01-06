507d2b75d705dce13571b3c5a8531a57645c7810
Chase Coney <chasiepoo@gmail.com>
2021-01-06 00:05:08 -0600

    feat(signup): add form descriptions

--- resources/views/signup/partials/form.blade.php
+++ resources/views/signup/partials/form.blade.php
@@ -47,6 +47,7 @@
             <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                 <label for="steam_friend_code" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
                     Steam Friend Code
+                    <p class="mt-1 text-gray-500">Find this in your "Add a Friend" area in Steam.</p>
                 </label>
                 <div class="mt-1 sm:mt-0 sm:col-span-2">
                     <input type="text" name="steam_friend_code" id="steam_friend_code" class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
@@ -58,6 +59,7 @@
                 <label for="f1_team_id"
                        class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
                     Desired Team
+                    <p class="mt-1 text-gray-500">Choose a team. For Reserve drivers, in practice, you will be assigned any available team.</p>
                 </label>
                 <div class="mt-1 sm:mt-0 sm:col-span-2">
                     <select id="f1_team_id" name="f1_team_id" autocomplete="f1_team_id"
@@ -79,6 +81,7 @@
                 <label for="f1_number_id"
                        class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
                     Desired Racing Number
+                    <p class="mt-1 text-gray-500">Choose an available in-game number to use for all official league races.</p>
                 </label>
                 <div class="mt-1 sm:mt-0 sm:col-span-2">
                     <select id="f1_number_id" name="f1_number_id" autocomplete="f1_number_id"
