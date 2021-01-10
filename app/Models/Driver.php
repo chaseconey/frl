5f0054c7338d40066b169e9a33b612e99a97b5d9
Chase Coney <chasiepoo@gmail.com>
2021-01-10 13:44:06 -0600

    feat(driver): add action eventing

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Nova\Actions\Actionable;
 
 class Driver extends Model
 {
-    use HasFactory;
+    use HasFactory, Actionable;
 
     protected $fillable = [
         'user_id',
