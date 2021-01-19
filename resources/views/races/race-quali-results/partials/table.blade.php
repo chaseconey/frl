2d8d1cb2b8887f8a27092780d2a685ff462f4db7
Chase Coney <chaseconey@users.noreply.github.com>
2021-01-19 19:31:27 +0000

    chore: add target blank to pov videos

--- resources/views/races/race-quali-results/partials/table.blade.php
+++ resources/views/races/race-quali-results/partials/table.blade.php
@@ -89,7 +89,7 @@
                             </td>
                             <td class="px-6 py-4 whitespace-nowrap text-center">
                                 @if($driverVideos->has($result->driver_id))
-                                    <a href="{{ $driverVideos->get($result->driver_id)->video_url }}" class="text-red-600 hover:text-red-900">
+                                    <a href="{{ $driverVideos->get($result->driver_id)->video_url }}" class="text-red-600 hover:text-red-900" target="_blank">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
                                         </svg>
