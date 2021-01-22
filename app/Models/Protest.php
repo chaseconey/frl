8138244d44ad736cca1cc7bd5b5e464794123a42
Chase Coney <chasiepoo@gmail.com>
2021-01-21 21:44:35 -0600

    feat(protest): protesting user will now get email on resolution

--- app/Models/Protest.php
+++ app/Models/Protest.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\ProtestReviewComplete;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -17,6 +18,29 @@ class Protest extends Model
         "description",
         "rules_breached"
     ];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::updating(function ($protest) {
+            $old = $protest->getOriginal();
+            $new = $protest->getDirty();
+
+            // If a decision is made, send notification to driver, but only the first time it is changed.
+            if (
+                is_null($old['stewards_decision']) &&
+                array_key_exists('stewards_decision', $new) &&
+                ! is_null($new['stewards_decision'])
+            ) {
+                $user = Driver::find($old['driver_id'])->user;
+                $user->notify(new ProtestReviewComplete);
+            }
+        });
+    }
 
     public function driver()
     {
---
+++ app/Notifications/ProtestReviewComplete.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Protest;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ProtestReviewComplete extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('A decision has been reached on your protest.')
+            ->action('Read Decision', route('profile.protests'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
