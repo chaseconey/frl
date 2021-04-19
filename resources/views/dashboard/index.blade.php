6b3ccab8adfb4dc6f4e6ddf94e4f2f14e1993398
Chase Coney <chasiepoo@gmail.com>
2021-04-18 20:39:54 -0500

    fix: dashbaord grid spacing

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,19 +5,21 @@
         </h2>
     </x-slot>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 py-12 max-w-7xl mx-auto">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 py-12">
 
-        @include('components.races-detail-list', ['races' => $latestRaces, 'title' => 'Latest Races'])
+            @include('components.races-detail-list', ['races' => $latestRaces, 'title' => 'Latest Races'])
 
-        @if(count($myRaces) > 0)
-            @include('components.races-detail-list', ['races' => $myRaces, 'title' => 'My Races'])
-        @else
-            @include('dashboard.partials.no-races')
-        @endif
+            @if(count($myRaces) > 0)
+                @include('components.races-detail-list', ['races' => $myRaces, 'title' => 'My Races'])
+            @else
+                @include('dashboard.partials.no-races')
+            @endif
 
-    </div>
+        </div>
 
-    <div class="grid grid-cols-1 pb-12 max-w-7xl mx-auto">
-        @include('dashboard.partials.feed', ['feed' => $feed])
+        <div class="grid grid-cols-1 pb-12">
+            @include('dashboard.partials.feed', ['feed' => $feed])
+        </div>
     </div>
 </x-app-layout>
