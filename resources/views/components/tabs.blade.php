d60d351bdd89e836d6fb200e8617cd4b710e3320
Chase Coney <chasiepoo@gmail.com>
2021-04-26 21:35:30 -0500

    fix(races): mobile tab dropdown

--- resources/views/components/tabs.blade.php
+++ resources/views/components/tabs.blade.php
@@ -7,6 +7,7 @@
             class="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
             onchange="location = this.value;"
         >
+            <option></option>
             @foreach ($links as $link)
             <option value="{{ $link['link'] }}">{{ $link['title'] }}</option>
             @endforeach
