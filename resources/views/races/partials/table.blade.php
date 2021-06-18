93e5f26eb339c6b4fe7d38eb5c6bb3046a80a4ea
Chase Coney <chasiepoo@gmail.com>
2021-06-18 12:01:51 -0500

    fix(race): broadcast icon sizing

--- resources/views/races/partials/table.blade.php
+++ resources/views/races/partials/table.blade.php
@@ -58,7 +58,7 @@
                         <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                             @if($race->broadcastVideos()->exists())
                             <a href="{{ route('races.broadcast', $race->id) }}" class="text-indigo-600 dark:text-indigo-400 hover:text-indigo-900">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
                                 </svg>
                             </a>
