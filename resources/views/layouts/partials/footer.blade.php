bbb03a74c3c21efa6f8090c0d2c95511da8a9537
Chase Coney <chasiepoo@gmail.com>
2021-06-18 16:30:31 -0500

    fix(footer): light mode bg color

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-gray-100 dark:bg-gray-700">
+<footer class="bg-white dark:bg-gray-700">
     <div class="max-w-7xl mx-auto py-8 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <div class="flex justify-center space-x-6 md:order-2">
 
