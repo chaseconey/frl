2f1f14a110e8e52b2d855f09fe3b22f6f40df5bf
Chase Coney <chaseconey@users.noreply.github.com>
2021-06-29 02:33:03 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Race/RaceResultsController.php
+++ app/Http/Controllers/Race/RaceResultsController.php
@@ -62,7 +62,7 @@ class RaceResultsController extends Controller
         $results = json_decode($json, true);
 
         try {
-            $this->uploadResults($results, $race, fn($results) => RaceResult::fromFile($results));
+            $this->uploadResults($results, $race, fn ($results) => RaceResult::fromFile($results));
         } catch (ResultsUploadError $e) {
             return response()->json([
                 'message' => $e->getMessage(),
