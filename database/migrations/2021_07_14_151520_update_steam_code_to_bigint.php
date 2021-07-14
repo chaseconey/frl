5ec448f02ed1a0714f807953edbed330cf4f5d89
Chase Coney <chasiepoo@gmail.com>
2021-07-14 10:17:58 -0500

    feat(driver): update steam code to bigint

---
+++ database/migrations/2021_07_14_151520_update_steam_code_to_bigint.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateSteamCodeToBigint extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('drivers', function (Blueprint $table) {
+            $table->unsignedBigInteger('steam_friend_code')->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('drivers', function (Blueprint $table) {
+            $table->unsignedInteger('steam_friend_code')->default(0)->change();
+        });
+    }
+}
