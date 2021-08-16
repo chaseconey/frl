389b0f3490703b7102a8ade5d658b8b528b3dcbb
Chase Coney <chasiepoo@gmail.com>
2021-08-16 18:25:34 -0500

    fix: home page f1 game year

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -118,10 +118,10 @@
                     <div class="sm:text-center lg:text-left">
                         <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white sm:text-5xl md:text-6xl">
                             <span class="block xl:inline">Formula Racing League</span>
-                            <span class="block text-indigo-600 dark:text-indigo-400 xl:inline">F1 2020</span>
+                            <span class="block text-indigo-600 dark:text-indigo-400 xl:inline">F1 2021</span>
                         </h1>
                         <p class="mt-3 text-base text-gray-500 dark:text-gray-200 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                            A multi-division racing league for the Formula 1 game F1 2020.
+                            A multi-division racing league for the Formula 1 game F1 2021.
                         </p>
                         <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                             <div class="rounded-md shadow">
