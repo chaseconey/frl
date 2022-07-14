c7bb769ede5d94b1eb24cd0945f2887112fbf2c3
Chase Coney <chasiepoo@gmail.com>
2022-07-13 20:50:39 -0500

    feat(race): allow searching by country name

--- app/ModelFilters/RaceFilter.php
+++ app/ModelFilters/RaceFilter.php
@@ -7,11 +7,11 @@ use EloquentFilter\ModelFilter;
 class RaceFilter extends ModelFilter
 {
     /**
-    * Related Models that have ModelFilters as well as the method on the ModelFilter
-    * As [relationMethod => [input_key1, input_key2]].
-    *
-    * @var array
-    */
+     * Related Models that have ModelFilters as well as the method on the ModelFilter
+     * As [relationMethod => [input_key1, input_key2]].
+     *
+     * @var array
+     */
     public $relations = [];
 
     public function division($id)
@@ -21,9 +21,13 @@ class RaceFilter extends ModelFilter
 
     public function search($search)
     {
-        return $this->whereHas('track', function ($query) use ($search) {
+
+        // Translate full country to shorthand (which is what the DB has)
+        $shorthand = array_search($search, config('countries')) ?? $search;
+
+        return $this->whereHas('track', function ($query) use ($shorthand, $search) {
             return $query->where('name', 'LIKE', "%{$search}%")
-                ->orWhere('country', 'LIKE', "%{$search}%");
+                ->orWhere('country', 'LIKE', "%{$shorthand}%");
         });
     }
 }
