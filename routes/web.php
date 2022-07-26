927b1a9c916453023e2c553fd8207e4d0deb195a
Shift <shift@laravelshift.com>
2022-07-26 15:30:13 +0000

    Shift to class based routes

--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,19 @@ Route::get('/', function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('divisions/{division}/team-standings', '\App\Http\Controllers\Division\StandingController@teamStandings')
+Route::get('divisions/{division}/team-standings', [\App\Http\Controllers\Division\StandingController::class, 'teamStandings'])
     ->name('standings.team-standings');
-Route::get('divisions/{division}/matrix', '\App\Http\Controllers\Division\StandingController@matrix')
+Route::get('divisions/{division}/matrix', [\App\Http\Controllers\Division\StandingController::class, 'matrix'])
     ->name('standings.matrix');
-Route::get('divisions/{division}/plot', '\App\Http\Controllers\Division\StandingController@plot')
+Route::get('divisions/{division}/plot', [\App\Http\Controllers\Division\StandingController::class, 'plot'])
     ->name('standings.plot');
 Route::resource('blog', \App\Http\Controllers\BlogController::class)->only(['index', 'show']);
 
 Route::resource('standings', \App\Http\Controllers\StandingController::class);
 
-Route::get('/api/races', '\App\Http\Controllers\Api\RaceController@index');
-Route::get('races/list', '\App\Http\Controllers\RaceController@list')->name('races.list');
-Route::get('races/{race}/broadcast', '\App\Http\Controllers\RaceController@broadcast')->name('races.broadcast');
+Route::get('/api/races', [\App\Http\Controllers\Api\RaceController::class, 'index']);
+Route::get('races/list', [\App\Http\Controllers\RaceController::class, 'list'])->name('races.list');
+Route::get('races/{race}/broadcast', [\App\Http\Controllers\RaceController::class, 'broadcast'])->name('races.broadcast');
 
 Route::resource('races', \App\Http\Controllers\RaceController::class)->only('index');
 Route::resource('race.results', \App\Http\Controllers\Race\RaceResultsController::class);
@@ -42,19 +42,19 @@ Route::middleware('auth')->group(function () {
     Route::get('/auth/discord', [\App\Http\Controllers\Auth\DiscordAuthController::class, 'redirect'])->name('auth.discord');
     Route::get('/auth/discord/callback', [\App\Http\Controllers\Auth\DiscordAuthController::class, 'callback']);
 
-    Route::get('dashboard', '\App\Http\Controllers\DashboardController@index')->name('dashboard');
+    Route::get('dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 
-    Route::get('profile/protests', '\App\Http\Controllers\ProfileController@protests')
+    Route::get('profile/protests', [\App\Http\Controllers\ProfileController::class, 'protests'])
         ->name('profile.protests');
 
-    Route::get('divisions/{division}/export', '\App\Http\Controllers\DivisionController@export')
+    Route::get('divisions/{division}/export', [\App\Http\Controllers\DivisionController::class, 'export'])
         ->name('standings.export');
 
     Route::resource('divisions', \App\Http\Controllers\DivisionController::class)->only('index');
     Route::resource('signup', \App\Http\Controllers\SignupController::class)->only('index', 'create', 'store');
     Route::resource('drivers', \App\Http\Controllers\DriverController::class)->only(['edit', 'update', 'show']);
     Route::resource('driver-videos', \App\Http\Controllers\DriverVideoController::class)->only(['store']);
-    Route::get('races/{race}/protests', '\App\Http\Controllers\Race\ProtestsController@index')->name('races.protests');
+    Route::get('races/{race}/protests', [\App\Http\Controllers\Race\ProtestsController::class, 'index'])->name('races.protests');
 
     Route::resource('protests', \App\Http\Controllers\ProtestController::class);
 });
