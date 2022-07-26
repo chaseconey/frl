6ac941b7e38f5eb3bc6f9859a2ad9de09a484a77
Shift <shift@laravelshift.com>
2022-07-26 16:36:58 +0000

    Rename ENV variables

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -31,7 +31,7 @@ import 'notyf/notyf.min.css';
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
